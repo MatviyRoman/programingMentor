bac19731f52399f3b0cfe2e1a814a8d6576f768f
Roman <romas6ka@i.ua>
2020-02-10 23:09:00 +0200

    Add files via upload

---
+++ day-15.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/572cb264362806af46000793/train/javascript
+
+function threeInOne(arr) {
+    console.log(arr.length);
+    for (let i = 3; i <= 0; i++) {
+      //     return arr.splice(i,3);
+      console.log(i + 3);
+    }
+  }
