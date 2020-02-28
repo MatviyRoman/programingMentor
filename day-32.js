7bbeba3887c2a3181207caf0ab50fdd2ba8d427b
Roman Matviy <roman@matviy.pp.ua>
2020-02-28 12:18:39 +0200

    day 32 fix error

--- day-32.js
+++ day-32.js
@@ -1,15 +1,21 @@
 // https://www.codewars.com/kata/547c71fdc5b2b38db1000098/train/javascript
 
 function NameMe(first, last) {
-    this.firstName = first,this.lastName = last;
-    //return {name: this.firstName + ' ' + this.lastName};
+  (this.firstName = first), (this.lastName = last);
+  //return {name: this.firstName + ' ' + this.lastName};
 }
 
+function NameMe(first, last) {
+  if (first && last !== '') {
+    return (this.firstName = first), (this.lastName = last);
+  }
+  return { name: this.firstName + ' ' + this.lastName };
+}
 
 // https://www.codewars.com/kata/this-is-an-other-problem
 
 function NamedOne(first, last) {
-    this.firstName = first;
-    this.lastName = last;
-    this.fullName = this.firstName + ' ' + this.lastName;
+  this.firstName = first;
+  this.lastName = last;
+  this.fullName = this.firstName + ' ' + this.lastName;
 }
