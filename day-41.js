a5f0ef169a8c198f73b0f822405fe3c257a08835
Roman Matviy <roman@matviy.pp.ua>
2020-03-08 00:43:48 +0200

    upload day-41.js

--- day-41.js
+++ day-41.js
@@ -1,3 +1,23 @@
 // https://www.codewars.com/kata/basics-generators-number-1/javascript
 
+function* generator() {
+    for (let x = 1; true; x++) {
+        yield x;
+    }
+}
+
 // https://www.codewars.com/kata/5637ead70013386e30000027/
+
+// function* generator(a) {
+
+// }
+
+function* generator() {
+    for (var x = 1; true; x++) {
+        yield '1 x ' + x + ' = ' + x;
+    }
+}
+
+// for (var x of generator()) {
+//   console.log(x)
+// }
